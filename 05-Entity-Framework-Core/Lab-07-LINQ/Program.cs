fabef9c12dac686a19e59f96e4a2caa6082fb90c
deepika063 <boreddydeepika37@gmail.com>
2026-07-09 19:30:57 +0530

    Completed EF Core Lab 7 - LINQ Queries

--- 05-Entity-Framework-Core/Lab-07-LINQ/Program.cs
+++ 05-Entity-Framework-Core/Lab-07-LINQ/Program.cs
@@ -1,1 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
 
+namespace RetailInventory
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var context = new AppDbContext();
+
+            Console.WriteLine("Products with Price > 1000");
+
+            var filteredProducts = await context.Products
+                .Where(p => p.Price > 1000)
+                .OrderByDescending(p => p.Price)
+                .ToListAsync();
+
+            foreach (var product in filteredProducts)
+            {
+                Console.WriteLine($"{product.Name} - ₹{product.Price}");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Product DTO");
+
+            var productDTOs = await context.Products
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Price
+                })
+                .ToListAsync();
+
+            foreach (var item in productDTOs)
+            {
+                Console.WriteLine($"{item.Name} - ₹{item.Price}");
+            }
+        }
+    }
+}
