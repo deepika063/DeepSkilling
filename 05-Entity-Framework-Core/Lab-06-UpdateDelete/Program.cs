745085e7233eff74fad1e6a701c2607b55fa58f1
deepika063 <boreddydeepika37@gmail.com>
2026-07-09 19:30:19 +0530

    Completed EF Core Lab 6 - Update and Delete Records

--- 05-Entity-Framework-Core/Lab-06-UpdateDelete/Program.cs
+++ 05-Entity-Framework-Core/Lab-06-UpdateDelete/Program.cs
@@ -1,1 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
 
+namespace RetailInventory
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var context = new AppDbContext();
+
+            // Update Product Price
+            var product = await context.Products
+                .FirstOrDefaultAsync(p => p.Name == "Laptop");
+
+            if (product != null)
+            {
+                product.Price = 70000;
+                await context.SaveChangesAsync();
+
+                Console.WriteLine("Laptop price updated successfully.");
+            }
+
+            // Delete Product
+            var deleteProduct = await context.Products
+                .FirstOrDefaultAsync(p => p.Name == "Rice Bag");
+
+            if (deleteProduct != null)
+            {
+                context.Products.Remove(deleteProduct);
+                await context.SaveChangesAsync();
+
+                Console.WriteLine("Rice Bag deleted successfully.");
+            }
+        }
+    }
+}
