cea91baebbc71d24c68725f392aa1d9a15f090fa
deepika063 <boreddydeepika37@gmail.com>
2026-07-09 21:02:54 +0530

    Create Program.cs

---
+++ 06-ASP.NET-Core-WebAPI/Kafka-Handson/ChatWinForms/Program.cs
@@ -0,0 +1,12 @@
+namespace ChatWinForms
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
