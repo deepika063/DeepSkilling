650f2c19f429bf74bfe74536bb855bf4cd75e688
deepika063 <boreddydeepika37@gmail.com>
2026-07-09 20:45:57 +0530

    Completed Web API Handson 3 - Custom Models, Filters and Exception Handling

---
+++ 06-ASP.NET-Core-WebAPI/WebAPI-Handson-3/Program.cs
@@ -0,0 +1,24 @@
+using WebAPIHandson.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<CustomExceptionFilter>();
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<CustomExceptionFilter>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run();
