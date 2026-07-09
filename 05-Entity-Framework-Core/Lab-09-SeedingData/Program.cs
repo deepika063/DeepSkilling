ca682dab54dcf72ceb2c83a407c0219035a2e5cc
deepika063 <boreddydeepika37@gmail.com>
2026-07-09 19:34:53 +0530

    Create Program.cs

---
+++ 05-Entity-Framework-Core/Lab-09-SeedingData/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seed data configured successfully.");
+        }
+    }
+}
