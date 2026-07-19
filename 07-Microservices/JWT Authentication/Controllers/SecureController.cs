6cf42f7c05442f19ccf2f71625839966044fbe5e
deepika063 <boreddydeepika37@gmail.com>
2026-07-19 19:27:38 +0530

    Create SecureController.cs

---
+++ 07-Microservices/JWT Authentication/Controllers/SecureController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthenticationDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureController : ControllerBase
+    {
+        [Authorize]
+
+        [HttpGet("data")]
+
+        public IActionResult GetSecureData()
+        {
+            return Ok("This is protected data.");
+        }
+    }
+}
