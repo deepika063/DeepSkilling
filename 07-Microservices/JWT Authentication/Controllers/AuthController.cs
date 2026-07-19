c2f5d8fc4bfa5d59056a1462a4aa99b17f14edaa
deepika063 <boreddydeepika37@gmail.com>
2026-07-19 19:27:12 +0530

    Create AuthController.cs

---
+++ 07-Microservices/JWT Authentication/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+using JWTAuthenticationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthenticationDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login(LoginModel model)
+        {
+            if (model.Username == "admin" &&
+                model.Password == "admin123")
+            {
+                var token = GenerateToken(model.Username);
+
+                return Ok(new
+                {
+                    Token = token
+                });
+            }
+
+            return Unauthorized();
+        }
+
+        private string GenerateToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role,"Admin")
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("ThisIsASecretKeyForJwtToken"));
+
+            var credentials = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+
+                issuer: "MyAuthServer",
+
+                audience: "MyApiUsers",
+
+                claims: claims,
+
+                expires: DateTime.Now.AddMinutes(60),
+
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
